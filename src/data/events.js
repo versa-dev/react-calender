c6dd5bb5a82e9e5f65cbd70caf4c34e13db0b388
versa-dev <tothefuture1117@gmail.com>
2020-11-09 09:19:49 +0800

    add Mock data and update the css for live and schedule event

---
+++ src/data/events.js
@@ -0,0 +1,69 @@
+const events = {
+  events: [
+    {
+      id: 1,
+      text: "Event 1",
+      start: "2020-11-01",
+      end: "2020-11-13",
+      resource: "A"
+    },
+    {
+      id: 2,
+      text: "Event 2",
+      start: "2020-11-13",
+      end: "2020-11-16",
+      resource: "B",
+    },
+    {
+      id: 3,
+      text: "Event 3",
+      start: "2020-11-07",
+      end: "2020-11-18",
+      resource: "C",
+    },
+    {
+      id: 4,
+      text: "Event 4",
+      start: "2020-11-20",
+      end: "2020-11-26",
+      resource: "B",
+    }
+    ,{
+      id: 5,
+      text: "Event 5",
+      start: "2020-11-15",
+      end: "2020-11-16",
+      resource: "C",
+    },
+    {
+      id: 6,
+      text: "Event 6",
+      start: "2020-11-14",
+      end: "2020-11-21",
+      resource: "A",
+    },
+    {
+      id: 7,
+      text: "Event 7",
+      start: "2020-11-21",
+      end: "2020-11-29",
+      resource: "C",
+    },
+    {
+      id: 8,
+      text: "Event 8",
+      start: "2020-11-07",
+      end: "2020-11-19",
+      resource: "B",
+    },
+    {
+      id: 9,
+      text: "Event 9",
+      start: "2020-11-20",
+      end: "2020-11-26",
+      resource: "A",
+    }
+  ]
+}
+
+export default events
--- src/scheduler/Scheduler.js
+++ src/scheduler/Scheduler.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {DayPilot, DayPilotScheduler} from "daypilot-pro-react"
+import events from '../data/events'
 import './Scheduler.css'
 
 function Scheduler () {
@@ -27,60 +28,37 @@ function Scheduler () {
     ],
   })
 
-  const events = {events: [
-    {
-      id: 1,
-      text: "Event 1",
-      start: "2020-11-01T00:00:00",
-      end: "2020-11-15T00:00:00",
-      resource: "A"
-    },
-    {
-      id: 2,
-      text: "Event 2",
-      start: "2020-11-13T00:00:00",
-      end: "2020-11-19T00:00:00",
-      resource: "B",
-    },
-    {
-      id: 3,
-      text: "Event 3",
-      start: "2020-11-07T00:00:00",
-      end: "2020-11-18T00:00:00",
-      resource: "C",
-    },
-    {
-      id: 4,
-      text: "Event 3",
-      start: "2020-11-20T00:00:00",
-      end: "2020-11-26T00:00:00",
-      resource: "B",
-    }
-  ]}
-
-  
+  const dayRef = useRef(null);
 
   useEffect(() => {
 
-    // this.Scheduler.scrollTo(DayPilot.Date.today());
-    // document.getElementsByClassName("scheduler_default_main")[0].scrollTo(DayPilot.Date.today());
-
-    let temp = {...config, ...events}
-    console.log(temp)
-    setConfig(temp)
-
+    // Scroll to today in the calender
+    if (dayRef.current) {
+      dayRef.current.control.scrollTo(DayPilot.Date.today())
+    }
 
     // load resource and event data
-     
-  }, [])
+    let new_events = {events: []}
+    events.events.map(item => {
+      let date_1 = new Date(item.start)
+      let date_2 = DayPilot.Date.today().toDateLocal()
+
+      if (date_1.getTime() > date_2.getTime()) {
+        item.backColor = "#e9a81e"
+      }
+      new_events.events.push(item)
+    })
+    let temp = {...config, ...new_events}
+    setConfig(temp)
+
+  }, [dayRef])
+
 
   return (
     <div>
       <DayPilotScheduler
         {...config}
-        // ref={component => {
-        //   this.scheduler = component && component.control;
-        // }}
+        ref={ref => dayRef.current = ref}
       />
     </div>
   );
