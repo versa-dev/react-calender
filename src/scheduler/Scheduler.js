cba10da352ebfb60f453c0aafbc7d94e064e36c5
versa-dev <tothefuture1117@gmail.com>
2020-11-09 08:22:41 +0800

    update to functional component from class component

--- src/scheduler/Scheduler.js
+++ src/scheduler/Scheduler.js
@@ -1,89 +1,89 @@
-import React, {Component} from 'react'
+import React, { useEffect, useState } from 'react'
 import {DayPilot, DayPilotScheduler} from "daypilot-pro-react"
 import './Scheduler.css'
 
-class Scheduler extends Component {
+function Scheduler () {
 
-  constructor(props) {
-    super(props);
+  const [config, setConfig] = useState({
+    timeHeaders: [{"groupBy": "Day", "format": "ddd, MM/d"}],
+    scale: "Day",
+    days: 30 + DayPilot.Date.today().getDay(),
+    cellWidth: 70,
+    rowHeaderWidthAutoFit: false,
+    startDate: DayPilot.Date.today().firstDayOfMonth(),
+    timeRangeSelectedHandling: "Disabled",
+    eventMoveHandling: "Disabled",
+    eventResizeHandling: "Disabled",
+    eventDeleteHandling: "Disabled",
+    eventClickHandling: "Disabled",
+    eventRightClickHandling: "Disabled",
+    rowHeaderColumns: [
+      { text: 'Slot #', display: "name" },
+    ],
+    resources: [
+      {name: "1", id: "A"},
+      {name: "2", id: "B"},
+      {name: "3", id: "C"},
+    ],
+  })
 
-    this.state = {
-      timeHeaders: [{"groupBy": "Day", "format": "ddd, MM/d"}],
-      scale: "Day",
-      days: 30 + DayPilot.Date.today().getDay(),
-      cellWidth: 70,
-      rowHeaderWidthAutoFit: false,
-      startDate: DayPilot.Date.today().firstDayOfMonth(),
-      timeRangeSelectedHandling: "Disabled",
-      eventMoveHandling: "Disabled",
-      eventResizeHandling: "Disabled",
-      eventDeleteHandling: "Disabled",
-      eventClickHandling: "Disabled",
-      eventRightClickHandling: "Disabled",
-    };
-  }
+  const events = {events: [
+    {
+      id: 1,
+      text: "Event 1",
+      start: "2020-11-01T00:00:00",
+      end: "2020-11-15T00:00:00",
+      resource: "A"
+    },
+    {
+      id: 2,
+      text: "Event 2",
+      start: "2020-11-13T00:00:00",
+      end: "2020-11-19T00:00:00",
+      resource: "B",
+    },
+    {
+      id: 3,
+      text: "Event 3",
+      start: "2020-11-07T00:00:00",
+      end: "2020-11-18T00:00:00",
+      resource: "C",
+    },
+    {
+      id: 4,
+      text: "Event 3",
+      start: "2020-11-20T00:00:00",
+      end: "2020-11-26T00:00:00",
+      resource: "B",
+    }
+  ]}
 
-  componentDidMount() {
+  
 
-    this.scheduler.scrollTo(DayPilot.Date.today());
+  useEffect(() => {
+
+    // this.Scheduler.scrollTo(DayPilot.Date.today());
+    // document.getElementsByClassName("scheduler_default_main")[0].scrollTo(DayPilot.Date.today());
+
+    let temp = {...config, ...events}
+    console.log(temp)
+    setConfig(temp)
+
 
     // load resource and event data
-    this.setState({
-      rowHeaderColumns: [
-        { text: 'Slot #', display: "name" },
-      ],
-      resources: [
-        {name: "1", id: "A"},
-        {name: "2", id: "B"},
-        {name: "3", id: "C"},
-      ],
-      events: [
-        {
-          id: 1,
-          text: "Event 1",
-          start: "2020-11-01T00:00:00",
-          end: "2020-11-15T00:00:00",
-          resource: "A"
-        },
-        {
-          id: 2,
-          text: "Event 2",
-          start: "2020-11-13T00:00:00",
-          end: "2020-11-19T00:00:00",
-          resource: "B",
-        },
-        {
-          id: 3,
-          text: "Event 3",
-          start: "2020-11-07T00:00:00",
-          end: "2020-11-18T00:00:00",
-          resource: "C",
-        },
-        {
-          id: 4,
-          text: "Event 3",
-          start: "2020-11-20T00:00:00",
-          end: "2020-11-26T00:00:00",
-          resource: "B",
-        }
-      ]
-    });
+     
+  }, [])
 
-  }
-
-  render() {
-    var {...config} = this.state;
-    return (
-      <div>
-        <DayPilotScheduler
-          {...config}
-          ref={component => {
-            this.scheduler = component && component.control;
-          }}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <DayPilotScheduler
+        {...config}
+        // ref={component => {
+        //   this.scheduler = component && component.control;
+        // }}
+      />
+    </div>
+  );
 }
 
-export default Scheduler;
+export default Scheduler
